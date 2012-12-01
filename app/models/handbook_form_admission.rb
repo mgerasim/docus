58f2b447a05c0e094ff18f375ee780414f8a83a4
Mikhail <mgerasim@inbox.ru>
2012-12-01 22:56:40 +1100

    review

--- app/models/handbook_form_admission.rb
+++ app/models/handbook_form_admission.rb
@@ -1,3 +1,10 @@
+#encoding: utf-8
 class HandbookFormAdmission < ActiveRecord::Base
   attr_accessible :name
+  HUMANIZED_ATTRIBUTES = {
+  	:name => "Наименование"
+  }
+  def self.human_attribute_name(attr, options={})
+	HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
 end
